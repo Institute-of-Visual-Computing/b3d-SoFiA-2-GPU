3f7b1645646ebf7df87f1f681037627f10c37159
IKE200 <david@bernhardt-nidderau.de>
2024-09-20 11:31:01 +0200

    Added argument to benchmark script to set the number of iterations

--- Benchmark.sh
+++ Benchmark.sh
@@ -4,12 +4,12 @@ touch benchresults.txt
 
 printf "std, negative, no scaling\n\nCPU:\n\n" >> benchresults.txt
 
-for i in {1..20};
+for i in {1..$1};
 do sofiaGPU sofia.par scfind.statistic=std pipeline.useGPU=false pipeline.benchmark=true;
 done;
 
 printf "\nGPU:\n\n" >> benchresults.txt
-for i in {1..20}
+for i in {1..$1}
 do 
 	sofiaGPU sofia.par scfind.statistic=std pipeline.useGPU=true pipeline.benchmark=true
 done;
@@ -17,14 +17,14 @@ done;
 printf "" >> benchresults.txt
 
 printf "\nmad, negative, no scaling\n\nCPU:\n\n" >> benchresults.txt
-for i in {1..20}
+for i in {1..$1}
 do
 	sofiaGPU sofia.par scfind.statistic=mad pipeline.useGPU=false pipeline.benchmark=true
 done
 
 printf "\nGPU:\n\n" >> benchresults.txt
 
-for i in {1..20}
+for i in {1..$1}
 do
 	sofiaGPU sofia.par scfind.statistic=mad pipeline.useGPU=true pipeline.benchmark=true
 done
@@ -32,12 +32,12 @@ done
 printf "" >> benchresults.txt
 
 printf "\ngauss, negative, no scaling\n\nCPU:\n\n" >> benchresults.txt
-for i in {1..20}
+for i in {1..$1}
 do
 	sofiaGPU sofia.par scfind.statistic=gauss pipeline.useGPU=false pipeline.benchmark=true
 done
 printf "\nGPU:\n\n" >> benchresults.txt
-for i in {1..20}
+for i in {1..$1}
 do
 	sofiaGPU sofia.par scfind.statistic=gauss pipeline.useGPU=true pipeline.benchmark=true
 done
